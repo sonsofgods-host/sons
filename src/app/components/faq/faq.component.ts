519c1cdf635264261dd170b3b282a21ea2482ade
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2022-02-25 20:10:54 -0300

    Fixed faq

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,33 +1,39 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.scss']
+  styleUrls: ['./faq.component.scss'],
 })
+export class FaqComponent {
+  activedCategory: string = 'Security';
 
+  activedQuestion: string = '';
 
-export class FaqComponent{
-  activedCategory : string = "Security";
-  
-  activateSecurity(){
-  this.activedCategory = "Security"
-  }
+  isShow: boolean = false;
 
-  activateMint(){
-    this.activedCategory = "Mint"
+  activateAnswer(answer: string) {
+    //Look if is showing a answer
+    if (this.activedQuestion != '') {
+      let active = document.getElementById(this.activedQuestion);
+      let active2 = document.getElementById(this.activedQuestion + '_answer');
+      if (active && active2) {
+        active.className = 'container_answer';
+        active2.className = 'answer';
+      }
     }
-
-  activateDates(){
-      this.activedCategory = "Dates"
+    let element = document.getElementById(answer);
+    let element2 = document.getElementById(answer + '_answer');
+    if (element && element2) {
+      if (answer != this.activedQuestion) {
+        element.className = 'container_answer show_answer_container';
+        element2.className = 'answer show_answer';
+        this.activedQuestion = answer;
+      } else {
+        element.className = 'container_answer';
+        element2.className = 'answer';
+        this.activedQuestion = '';
+      }
+    }
   }
-
-  activateDevelopment(){
-    this.activedCategory = "Development"
-  }
-
 }
-
-
-
-
