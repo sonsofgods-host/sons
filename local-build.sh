51a9ea8740a551182c862cb762522b07eb594102
Juan Toledo <juansebastiantoledofreire@hotmail.com>
2023-12-20 19:07:41 -0300

    Build production

--- local-build.sh
+++ local-build.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
 # Contenido de local_build.sh
+#!/bin/bash
+
+# Cambiar al directorio de la aplicación
+cd src
+
+# Instalar dependencias
 npm install
+
+# Construir la aplicación
 ng build
-# echo "33k.site" > docs/CNAME  # Ajusta "your-app-name" al nombre de tu aplicación Angular
+
+# Regresar al directorio raíz
+cd ..
+
+# Añadir el CNAME si es necesario
+# echo "33k.site" > docs/CNAME
+
+# Añadir todos los cambios al área de preparación
 git add .
+
+# Realizar el commit
 git commit -m "Build production"
+
+# Subir los cambios al repositorio remoto
 git push origin main
